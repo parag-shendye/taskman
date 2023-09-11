88b97516c1eefb833cd2c20f6838322f66c37cf7
parag shendye <paragshendye@gmail.com>
2023-09-11 22:54:20 +0200

    selecting tasks from tasklist

--- include/Helpers.h
+++ include/Helpers.h
@@ -9,17 +9,17 @@ namespace Helpers
     void showCreateForm(bool *poe);
     static void ShowCrudWindow(bool *p_open, CreateTaskModal &modal);
     static void ShowExampleAppLog(bool *p_open);
+    bool openTaskPopup = false;
     static void showFullWindows()
     {
         static bool use_work_area = true;
-        static bool open_create = false;
         static ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings;
         const ImGuiViewport *viewport = ImGui::GetMainViewport();
         ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos : viewport->Pos);
         ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize : viewport->Size);
         bool running = true;
         bool p_open = true;
-        if (ImGui::Begin("Example: Fullscreen window", &running, flags))
+        if (ImGui::Begin("Main", &running, flags))
         {
             if (ImGui::BeginTabBar("##Tabs", ImGuiTabBarFlags_None))
             {
@@ -35,12 +35,54 @@ namespace Helpers
                 if (ImGui::BeginTabItem("Task Management"))
                 {
                     ImGui::Text("Here you can Add, remove, modify your tasks");
-                    // if (ImGui::Button("CREATE", ImVec2(200, 70)))
-                    // {
-                    //     Helpers::showCreateForm(&p_open);
-                    // }
                     ImGui::NewLine();
+                    if (ImGui::BeginMenuBar())
+                    {
+                        if (ImGui::BeginMenu("File"))
+                        {
+                            if (ImGui::MenuItem("Close")){
+                                p_open = false;
+                            }
+                            ImGui::EndMenu();
+                        }
+                        ImGui::EndMenuBar();
+                    }
+                    static int selected = 0;
+                    {
+                        ImGui::BeginChild("left pane", ImVec2(150, 0), true);
+                        for (int i = 0; i < 100; i++)
+                        {
+                            // FIXME: Good candidate to use ImGuiSelectableFlags_SelectOnNav
+                            char label[128];
+                            sprintf(label, "Task %d", i);
+                            if (ImGui::Selectable(label, selected == i))
+                                {
+                                    selected = i;
+                                    openTaskPopup = true;
+                                }
+                        }
+                        ImGui::EndChild();
+                    }
+                    if (openTaskPopup)
+                    {
+                        ImGui::OpenPopup("Task Description Popup");
+                        openTaskPopup = false;
+                    }
+                    if (ImGui::BeginPopup("Task Description Popup"))
+                    {
+                        //TODO: here make the query
+                        std::string description = "kdfksdfkdsmfmsdkmf_"+ std::to_string(selected);
+                        ImGui::Text("Task Description:");
+                        ImGui::TextWrapped("%s", description.c_str());
+                        if (ImGui::Button("OK"))
+                        {
+                            ImGui::CloseCurrentPopup();
+                        }
+                        ImGui::EndPopup();
+                    }
+                    ImGui::SameLine();
                     ShowCrudWindow(&p_open, modal);
+                    // ImGui::ShowDemoWindow(&p_open);
                     ImGui::EndTabItem();
                 }
                 ImGui::EndTabBar();
@@ -78,7 +120,7 @@ namespace Helpers
                     std::cout << modal.descriptionText << std::endl;
                     ImGui::CloseCurrentPopup();
                     modal.isOpen = false;
-                }
+                }                
 
                 ImGui::EndPopup();
             }
